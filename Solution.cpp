544c7a9fec4df1293f0158b97fe55b9a00064f09
Bhavyasai2004 <112572100+Bhavyasai2004@users.noreply.github.com>
2023-07-16 17:55:38 +0530

    Solution.cpp

---
+++ Solution.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h> 
+char firstNonRepeatingCharacter(string str) {
+
+  // Write your code here
+  unordered_map<char,int>m;
+  for(int i=0;i<str.length();i++)
+  {
+    m[str[i]]++;
+  }
+  for(int i=0;i<str.length();i++)
+  {
+    if(m[str[i]]==1)
+    return str[i];
+  }
+  return str[0];
+}
